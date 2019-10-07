40e7f7a96effa02572d208a87365df140f877348
unknown <zendran.m@gmail.com>
2019-10-07 15:47:16 +0200

    Extracting mp4 to frames works

--- Scripts/unpack.py
+++ Scripts/unpack.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import cv2
 
 # Constants
 root_folder = 'C:/Users/michal/Desktop/data/'
@@ -16,19 +17,29 @@ def prepare_directory(base_folder, new_folder_name):
     os.mkdir(base_folder + 'src/')
 
 def copy_and_rename_videos(old_src_folder, new_src_folder):
-    i = 0
+    counter = 0
     subfolders = os.listdir(old_src_folder)
     for subfolder in subfolders:
         files = os.listdir(old_src_folder + subfolder + '/')
         for file_name in files:
-            i = i + 1
+            counter += 1
             shutil.copy(old_src_folder + subfolder + '/' + file_name, new_src_folder)
-            os.rename(new_src_folder + file_name, new_src_folder + "src_video_" + str(i) + '.mp4')
+            os.rename(new_src_folder + file_name, new_src_folder + "src_video_" + str(counter) + '.mp4')
 
+def extract_frames_from_video(video_path, video_name):
+    vidcap = cv2.VideoCapture(video_path + video_name)
+    success, image = vidcap.read()
+    counter = 0
+    while success:
+        cv2.imwrite(video_path + "frame%d.jpg" % counter, image)
+        success,image = vidcap.read()
+        print('Read a new frame: ', success)
+        counter += 1
 
+# prepare_directory(root_folder, 'src/')
+# copy_and_rename_videos(src_folder, root_folder + 'src/')
 
-prepare_directory(root_folder, 'src/')
-copy_and_rename_videos(src_folder, root_folder + 'src/')
+extract_frames_from_video(root_folder + 'src/', 'src_video_32.mp4')
 
 # Here decompose every mp4 file into single frames
 
