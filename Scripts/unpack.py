329a6d825364067fad10d765bad59b79bd56cd80
unknown <zendran.m@gmail.com>
2019-10-07 15:12:23 +0200

    Refactoring of unpack.py

--- Scripts/unpack.py
+++ Scripts/unpack.py
@@ -1,24 +1,35 @@
 import os
 import shutil
 
-base_folder = 'C:/Users/michal/Desktop/data/'
-src_folder = base_folder + 'id02019/'
+# Constants
+root_folder = 'C:/Users/michal/Desktop/data/'
+src_folder = root_folder + 'id02019/'
 
-if os.path.isdir(base_folder + 'src/') == True:
-    print('exist')
-    shutil.rmtree(base_folder + 'src/')
-else:
-    print('no exists')
+# Functions
+def prepare_directory(base_folder, new_folder_name):
+    if os.path.isdir(base_folder + new_folder_name) == True:
+        print('exist')
+        shutil.rmtree(base_folder + new_folder_name)
+    else:
+        print('no exists')
 
-os.mkdir(base_folder + 'src/')
+    os.mkdir(base_folder + 'src/')
 
-i = 0
+def copy_and_rename_videos(old_src_folder, new_src_folder):
+    i = 0
+    subfolders = os.listdir(old_src_folder)
+    for subfolder in subfolders:
+        files = os.listdir(old_src_folder + subfolder + '/')
+        for file_name in files:
+            i = i + 1
+            shutil.copy(old_src_folder + subfolder + '/' + file_name, new_src_folder)
+            os.rename(new_src_folder + file_name, new_src_folder + "src_video_" + str(i) + '.mp4')
 
-src_folders = os.listdir(src_folder)
-for subfolder in src_folders:
-    src_files = os.listdir(src_folder + subfolder + '/')
-    for file_name in src_files:
-        i = i + 1
-        print(subfolder)
-        shutil.copy(src_folder + subfolder + '/' + file_name, base_folder + 'src/')
-        os.rename(base_folder + 'src/' + file_name, base_folder + 'src/' + "src_img_" + str(i) + '.mp4')
+
+
+prepare_directory(root_folder, 'src/')
+copy_and_rename_videos(src_folder, root_folder + 'src/')
+
+# Here decompose every mp4 file into single frames
+
+# Here cut only face out of every frame
