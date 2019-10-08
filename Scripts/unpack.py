84cbd8788bd3143fcf8bd68a5a1b68e0f3b72ca3
unknown <zendran.m@gmail.com>
2019-10-08 16:49:23 +0200

    Functions refactoring

--- Scripts/unpack.py
+++ Scripts/unpack.py
@@ -16,31 +16,30 @@ def prepare_directory(base_folder, new_folder_name):
 
     os.mkdir(base_folder + 'src/')
 
-def copy_and_rename_videos(old_src_folder, new_src_folder):
-    counter = 0
+def extract_and_save_frames_from_video(video_path, dest_path, counter):
+    vidcap = cv2.VideoCapture(video_path)
+    success, image = vidcap.read()
+    while success:
+        cv2.imwrite(dest_path + "frame%d.png" % counter, image)
+        success,image = vidcap.read()
+        counter += 1
+    return counter
+
+def process_videos(old_src_folder, new_src_folder):
+    frame_counter = 1
+    folder_counter = 1
+    file_counter = 1
     subfolders = os.listdir(old_src_folder)
     for subfolder in subfolders:
         files = os.listdir(old_src_folder + subfolder + '/')
         for file_name in files:
-            counter += 1
-            shutil.copy(old_src_folder + subfolder + '/' + file_name, new_src_folder)
-            os.rename(new_src_folder + file_name, new_src_folder + "src_video_" + str(counter) + '.mp4')
+            video_path = old_src_folder + subfolder + '/' + file_name
+            print("Folder: " + str(folder_counter) + ", Video: " + str(file_counter) + ", Name: " + file_name)
+            frame_counter = extract_and_save_frames_from_video(video_path, new_src_folder, frame_counter)
+            file_counter += 1
+        folder_counter += 1
 
-def extract_frames_from_video(video_path, video_name):
-    vidcap = cv2.VideoCapture(video_path + video_name)
-    success, image = vidcap.read()
-    counter = 0
-    while success:
-        cv2.imwrite(video_path + "frame%d.jpg" % counter, image)
-        success,image = vidcap.read()
-        print('Read a new frame: ', success)
-        counter += 1
-
-# prepare_directory(root_folder, 'src/')
-# copy_and_rename_videos(src_folder, root_folder + 'src/')
-
-extract_frames_from_video(root_folder + 'src/', 'src_video_32.mp4')
-
-# Here decompose every mp4 file into single frames
+prepare_directory(root_folder, 'src/')
+process_videos(src_folder , root_folder + 'src/' )
 
 # Here cut only face out of every frame
