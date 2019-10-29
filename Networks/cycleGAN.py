65d3b6cbc7648a5c20d808b59afd458342925ad3
unknown <zendran.m@gmail.com>
2019-10-29 14:34:16 +0100

    cycleGAN load data improvements

--- Networks/cycleGAN.py
+++ Networks/cycleGAN.py
@@ -25,7 +25,7 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE # Whats that???
 
 # Load data
 def loadImages(src_dir, size=(IMG_HEIGHT, IMG_WIDTH)):
-    images = []
+    images = list()
     files = glob.glob(src_dir + "*.PNG")
     for filename in files:
         pixels = tf.keras.preprocessing.image.load_img(filename, target_size=size)
@@ -33,34 +33,21 @@ def loadImages(src_dir, size=(IMG_HEIGHT, IMG_WIDTH)):
         images.append(pixels)
     return images
 
-train_A = loadImages(train_person_A)
-test_A = loadImages(test_person_A)
-train_B = loadImages(train_person_B)
-test_B = loadImages(test_person_B)
-
 # Preprocess data
 def normalize(image):
   image = tf.cast(image, tf.float32)
   image = (image - 127.5) / 127.5
   return image
 
+train_A = loadImages(train_person_A)
+test_A = loadImages(test_person_A)
+train_B = loadImages(train_person_B)
+test_B = loadImages(test_person_B)
+
 train_A = tf.data.Dataset.from_tensor_slices(train_A).map(normalize, num_parallel_calls=AUTOTUNE).cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 test_A = tf.data.Dataset.from_tensor_slices(test_A).map(normalize, num_parallel_calls=AUTOTUNE).cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 train_B = tf.data.Dataset.from_tensor_slices(train_B).map(normalize, num_parallel_calls=AUTOTUNE).cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 test_B = tf.data.Dataset.from_tensor_slices(test_B).map(normalize, num_parallel_calls=AUTOTUNE).cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
-
-# # Test data
-# sample_A = next(iter(train_A))
-# sample_B = next(iter(train_B))
-
-# plt.subplot(121)
-# plt.title('Horse')
-# plt.imshow(sample_A[0] * 0.5 + 0.5)
-
-# plt.subplot(122)
-# plt.title('Horse')
-# plt.imshow(sample_B[0] * 0.5 + 0.5)
-# plt.show()
 
 # Import models
 from Generators.generator import make_generator_model
@@ -71,7 +58,7 @@ generator_f = make_generator_model()
 discriminator_x = make_discriminator_model()
 discriminator_y = make_discriminator_model()
 
-# Show generated samples
+# Generate and show samples
 sample_A = next(iter(train_A))
 sample_B = next(iter(train_B))
 
@@ -86,7 +73,6 @@ title = ['A', 'To B', 'B', 'To A']
 for i in range(len(imgs)):
   plt.subplot(2, 2, i+1)
   plt.title(title[i])
-  # print(imgs[i][0])
   if i % 2 == 0:
     plt.imshow(imgs[i][0] * 0.5 + 0.5)
   else:
