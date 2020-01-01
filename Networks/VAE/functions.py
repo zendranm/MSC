6a8c9a5c24f33def077afc856fcc2178c35cd8f6
unknown <zendran.m@gmail.com>
2020-01-01 22:13:32 +0100

    Working on VAE

---
+++ Networks/VAE/functions.py
@@ -0,0 +1,51 @@
+# Libraries
+import tensorflow as tf
+import tensorflow_probability as tfp
+import numpy as np
+
+tfd = tfp.distributions
+
+# Load data
+def load_data(filename):
+    data = np.load(filename)
+    X1, X2 = data['arr_0'], data['arr_1']
+    X1 = (X1 - 127.5) / 127.5
+    X2 = (X2 - 127.5) / 127.5
+    return [X1, X2]
+
+# Encoder
+def make_encoder(data, code_size):
+    x = tf.keras.layers.Flatten()(data)
+    x = tf.keras.layers.Dense(200, tf.nn.relu)(x)
+    x = tf.keras.layers.Dense(200, tf.nn.relu)(x)
+    loc = tf.keras.layers.Dense(code_size)(x)
+    scale = tf.keras.layers.Dense(code_size, tf.nn.softplus)(x)
+    return tfd.MultivariateNormalDiag(loc, scale)
+
+# Prior
+def make_prior(code_size):
+    loc = tf.zeros(code_size)
+    scale = tf.ones(code_size)
+    return tfd.MultivariateNormalDiag(loc, scale)
+
+# Decoder
+def make_decoder(code, data_shape):
+    x = code
+    x = tf.keras.layers.Dense(200, tf.nn.relu)(x)
+    x = tf.keras.layers.Dense(200, tf.nn.relu)(x)
+    logit = tf.keras.layers.Dense(np.prod(data_shape))(x)
+    logit = tf.reshape(logit, [-1] + data_shape)
+    return tfd.Independent(tfd.Bernoulli(logit), 2)
+
+def plot_codes(ax, codes, labels):
+    ax.scatter(codes[:, 0], codes[:, 1], s=2, c=labels, alpha=0.1)
+    ax.set_aspect('equal')
+    ax.set_xlim(codes.min() - .1, codes.max() + .1)
+    ax.set_ylim(codes.min() - .1, codes.max() + .1)
+    ax.tick_params(axis='both', which='both', left='off', bottom='off', labelleft='off', labelbottom='off')
+
+
+def plot_samples(ax, samples):
+    for index, sample in enumerate(samples):
+        ax[index].imshow(sample, cmap='gray')
+        ax[index].axis('off')
--- Networks/VAE/vae.py
+++ Networks/VAE/vae.py
@@ -1,11 +1,58 @@
-# Libraries
+# Libraries and functions
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+tfd = tfp.distributions
+
+from functions import load_data
+from functions import make_encoder
+from functions import make_prior
+from functions import make_decoder
+from functions import plot_codes
+from functions import plot_samples
 
 # Load and prepare data
+data_file = 'C:/Users/michal/Desktop/DiCaprioToDowneyJr.npz'
+dataset = load_data(data_file)
+image_shape = dataset[0].shape[1:]
+print(image_shape)
+print('Loaded', dataset[0].shape, dataset[1].shape)
 
 # Encoder
+make_encoder = tf.compat.v1.make_template('encoder', make_encoder)
+
+# Prior
+prior = make_prior(code_size=2)
 
 # Decoder 1
+make_decoder = tf.compat.v1.make_template('decoder', make_decoder)
 
 # Decoder 2
 
+# Loss
+tf.compat.v1.disable_eager_execution()
+data = tf.compat.v1.placeholder(tf.float32, [None, 160, 160, 3])
+
+prior = make_prior(code_size=2)
+posterior = make_encoder(data, code_size=2)
+code = posterior.sample()
+
+likelihood = make_decoder(code, [160, 160, 3]).log_prob(data)
+divergence = tfd.kl_divergence(posterior, prior)
+elbo = tf.reduce_mean(likelihood - divergence)
+
 # Train
+optimize = tf.compat.v1.train.AdamOptimizer(0.001).minimize(-elbo)
+samples = make_decoder(prior.sample(4), [160, 160, 3]).mean()
+
+with tf.compat.v1.train.MonitoredSession() as sess:
+  for epoch in range(20):
+    test_elbo, test_codes, test_samples = sess.run(
+        [elbo, code, samples], {data: dataset})
+    print('Epoch', epoch, 'elbo', test_elbo)
+    plot_codes(test_codes)
+    plot_samples(test_samples)
+    for _ in range(600):
+      sess.run(optimize, {data: dataset.train.next_batch(100)[0]})
+
+print("Program finished successfully!")
